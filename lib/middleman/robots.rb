dbefd2b914e676e2d35d396280fb951d98382ea0
yterajima <terra@e2esound.com>
2014-12-10 14:27:27 +0900

    first commit

---
+++ lib/middleman/robots.rb
@@ -0,0 +1,7 @@
+require "middleman/robots/version"
+
+module Middleman
+  module Robots
+    # Your code goes here...
+  end
+end
---
+++ lib/middleman/robots/version.rb
@@ -0,0 +1,5 @@
+module Middleman
+  module Robots
+    VERSION = "0.0.1"
+  end
+end
