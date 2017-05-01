1e05a009b3adab8c754fc4313ba3aa802e7b76ae
Yuya Matsushima <yuyam@fillin-inc.com>
2017-05-02 04:05:05 +0900

    Changed to use Tempfile
    - remove template file in extension.
    - now, using Tempfile
    - remove unnecessary files

--- lib/middleman-robots/extension.rb
+++ lib/middleman-robots/extension.rb
@@ -1,4 +1,5 @@
 require 'middleman-robots/generator'
+require 'tempfile'
 
 module Middleman
   module Robots
@@ -7,25 +8,19 @@ module Middleman
       option :rules, [], 'List of rules about sitemap.xml'
       option :sitemap, false, 'URI of sitemap.xml'
 
-      def initialize(app, options_hash = {}, &block)
-        super
-      end
-
       def manipulate_resource_list(resources)
-        data   = Generator.new(options.rules, options.sitemap).process
+        tf = Tempfile.open('middleman-robots')
+        tf.puts(Generator.new(options.rules, options.sitemap).process)
+
         robots = Middleman::Sitemap::Resource.new(
           app.sitemap,
           'robots.txt',
-          File.join(template_dir, 'robots.txt.erb')
+          tf.path
         )
-        robots.add_metadata(locals: {robots: data})
+        tf.close
 
         logger.info '== middleman-robots: robots.txt added to resources =='
         resources << robots
-      end
-
-      def template_dir
-        File.expand_path(File.join('..', 'templates'), __FILE__)
       end
     end
   end
