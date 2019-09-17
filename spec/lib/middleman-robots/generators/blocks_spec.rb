833cbd8b251fd54d4e6fbdcd50da8e4af90371a9
Yuya Matsushima <yuyam@fillin-inc.com>
2019-09-17 22:16:12 +0900

    Add Blocks class spec

--- spec/lib/middleman-robots/generators/blocks_spec.rb
+++ spec/lib/middleman-robots/generators/blocks_spec.rb
@@ -1,44 +1,48 @@
 require 'middleman-robots/generators/blocks'
 
 RSpec.describe Middleman::Robots::Generators::Blocks do
-  let(:rules) do
-    [
-      {
-        user_agent: 'GoogleBot',
-        disallow: %w[tmp/* /someting/dir/disallow.html],
-        allow: %w[allow/* /someting/dir/allow.html]
-      },
-      {
-        user_agent: 'GoogleBot-Image',
-        disallow: %w[tmp/* /someting/dir/disallow.html],
-        allow: %w[allow/* /someting/dir/allow.html]
-      }
-    ]
-  end
-
   describe '#text' do
-    subject do
-      klass = described_class.new(rules)
-      klass.text
+    subject { described_class.new(rules).text }
+
+    context 'with all options' do
+      let(:rules) do
+        [
+          {
+            user_agent: 'GoogleBot',
+            disallow: %w[tmp/* /someting/dir/disallow.html],
+            allow: %w[allow/* /someting/dir/allow.html]
+          },
+          {
+            user_agent: 'GoogleBot-Image',
+            disallow: %w[tmp/* /someting/dir/disallow.html],
+            allow: %w[allow/* /someting/dir/allow.html]
+          }
+        ]
+      end
+      let(:expected) do
+        expected =<<~ROBOTS
+          User-Agent: GoogleBot
+          Disallow: /tmp/*
+          Disallow: /someting/dir/disallow.html
+          Allow: /allow/*
+          Allow: /someting/dir/allow.html
+
+          User-Agent: GoogleBot-Image
+          Disallow: /tmp/*
+          Disallow: /someting/dir/disallow.html
+          Allow: /allow/*
+          Allow: /someting/dir/allow.html
+        ROBOTS
+        expected.chomp
+      end
+
+      it { is_expected.to eq expected }
     end
 
-    let(:expected) do
-      expected =<<~ROBOTS
-        User-Agent: GoogleBot
-        Disallow: /tmp/*
-        Disallow: /someting/dir/disallow.html
-        Allow: /allow/*
-        Allow: /someting/dir/allow.html
+    context 'without option' do
+      let(:rules) { {} }
 
-        User-Agent: GoogleBot-Image
-        Disallow: /tmp/*
-        Disallow: /someting/dir/disallow.html
-        Allow: /allow/*
-        Allow: /someting/dir/allow.html
-      ROBOTS
-      expected.chomp
+      it { is_expected.to be_empty }
     end
-
-    it { is_expected.to eq expected.chomp }
   end
 end
